394bada044af6db14f097bc3cf711fdcead5366c
DavePhil <dave.chedjoun@facsciences-uy1.cm>
2023-10-09 14:11:50 +0100

    Redaction de la classe de Test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+/*
+Fait par CHEDJOUN KENGUEP Dave Master1 spécialité Genie Logiciel
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("************************ Debut ************************");
+        int somme = Arithmetique.getInstance().somme(5, 4);
+        System.out.println("La somme est: "+ somme);
+        Arithmetique a1 = Arithmetique.getInstance(9,3);
+        a1.affiche();
+        Arithmetique a2 = Arithmetique.getInstance(1,2);
+        a2.affiche();
+        System.out.println("************************* Fin *************************");
     }
 }
